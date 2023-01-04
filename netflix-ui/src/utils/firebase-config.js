952c145dc362ad1c96899f634b25eef6e57f40f0
HZhu02 <zhu020205@gmail.com>
2023-01-03 22:06:53 -0800

    Update firebase-config.js

--- netflix-ui/src/utils/firebase-config.js
+++ netflix-ui/src/utils/firebase-config.js
@@ -2,14 +2,15 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1Hf0_rdWLBzDPJPcO9CJN4y6M6-EgKH4",
-  authDomain: "react-auth-6788d.firebaseapp.com",
-  projectId: "react-auth-6788d",
-  storageBucket: "react-auth-6788d.appspot.com",
-  messagingSenderId: "131797845021",
-  appId: "1:131797845021:web:3f7ff4766e2b89ca5d32f4",
-  measurementId: "G-VWPBR1NSLL",
+  apiKey: "AIzaSyBd31AO26FF4sT6XY0MVMspihBoIujAOeY",
+  authDomain: "react-loveflix.firebaseapp.com",
+  projectId: "react-loveflix",
+  storageBucket: "react-loveflix.appspot.com",
+  messagingSenderId: "105562514171",
+  appId: "1:105562514171:web:99fc8606e7a1726a18c65e",
+  measurementId: "G-CKQD2WB6VK"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
